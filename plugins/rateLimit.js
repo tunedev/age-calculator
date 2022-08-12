bc7e38562714972017f870d51064e62d0e39b6be
Babatunde Sanusi <swisskid95@gmail.com>
2022-08-12 20:29:49 +0100

    try fix for proxy setup

--- plugins/rateLimit.js
+++ plugins/rateLimit.js
@@ -6,16 +6,14 @@ module.exports = fp(async function (fastify, opts) {
   fastify.register(require("@fastify/rate-limit"), {
     max: 3,
     timeWindow: 1000,
-    addHeadersOnExceeding: {
-      "x-ratelimit-limit": false,
-      "x-ratelimit-remaining": false,
-      "x-ratelimit-reset": false,
-    },
-    addHeaders: {
-      "x-ratelimit-limit": false,
-      "x-ratelimit-remaining": false,
-      "x-ratelimit-reset": false,
-      "retry-after": false,
+    keyGenerator: function (request) {
+      return (
+        request.headers["x-real-ip"] || // nginx
+        request.headers["x-client-ip"] || // apache
+        request.headers["x-forwarded-for"] || // use this only if you trust the header
+        request.session.username || // you can limit based on any session value
+        request.ip
+      );
     },
   });
 });
