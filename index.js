833a54f01dd4eb5063d4ab1063a37b403a3d3231
Babatunde Sanusi <swisskid95@gmail.com>
2022-08-12 19:59:25 +0100

    add ratelimit to the index.js file

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const AutoLoad = require("@fastify/autoload");
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
+  fastify.register(require("@fastify/rate-limit"), {
+    max: 3,
+    timeWindow: 1000,
+  });
 
   // Do not touch the following lines
 
