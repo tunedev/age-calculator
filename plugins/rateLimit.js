d2dee19b786fafab743536c7d17849454e85f713
Babatunde Sanusi <swisskid95@gmail.com>
2022-08-12 20:19:49 +0100

    remove header to ensure rate limit throws error

--- plugins/rateLimit.js
+++ plugins/rateLimit.js
@@ -6,5 +6,16 @@ module.exports = fp(async function (fastify, opts) {
   fastify.register(require("@fastify/rate-limit"), {
     max: 3,
     timeWindow: 1000,
+    addHeadersOnExceeding: {
+      "x-ratelimit-limit": false,
+      "x-ratelimit-remaining": false,
+      "x-ratelimit-reset": false,
+    },
+    addHeaders: {
+      "x-ratelimit-limit": false,
+      "x-ratelimit-remaining": false,
+      "x-ratelimit-reset": false,
+      "retry-after": false,
+    },
   });
 });
